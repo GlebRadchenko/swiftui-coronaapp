c4b2b211ca811f81ff2f2b95b04320cb29ccb737
Gleb Radchenko <gleb.radchenko@vw-dilab.com>
2020-11-29 14:54:02 +0100

    init project

---
+++ CoronaApp/CoronaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaApp
+//
+//  Created by Gleb Radchenko on 29.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoronaApp/CoronaApp/CoronaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronaAppApp.swift
+//  CoronaApp
+//
+//  Created by Gleb Radchenko on 29.11.20.
+//
+
+import SwiftUI
+
+@main
+struct CoronaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
